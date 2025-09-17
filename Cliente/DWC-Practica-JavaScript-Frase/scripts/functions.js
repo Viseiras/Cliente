a67789069c13acba42705427abc62fc99f89d43f
Javier Vicedo <101125117+Viseiras@users.noreply.github.com>
2025-09-17 19:01:45 +0200

    Add files via upload

--- Cliente/DWC-Practica-JavaScript-Frase/scripts/functions.js
+++ Cliente/DWC-Practica-JavaScript-Frase/scripts/functions.js
@@ -1,30 +1,72 @@
 'use strict'
 
 function letters(cadena) {
-
+	return cadena.length;
 }
 
 function words(cadena) {
-
+	let palabras = cadena.split(" ");
+	return palabras.length;
 }
 
 function upperString(cadena) {
-
+	return cadena.toUpperCase();
 }
 
 function titleString(cadena) {
-
+	let cadenaTitulo = "";
+	let palabras = cadena.split(" ");
+	for(let i=0;i<palabras.length;i++){
+		let palabra=palabras[i].split("");
+		for(let j=0;j<palabras[i].length;j++){
+			if(j===0){
+				cadenaTitulo+=palabra[j].toUpperCase();
+			}
+			else{
+				cadenaTitulo+=palabra[j].toLowerCase();
+			}
+		}
+		cadenaTitulo+=" ";
+	}
+	return cadenaTitulo;
 }
 
 function backwardsLetters(cadena) {
-
+	let cadenaInversa = "";
+	let palabras = cadena.split(" ");
+	for(let i=0;i<palabras.length;i++){
+		let palabra=palabras[i].split("");
+		for(let j=palabras[i].length;j>0;j--){
+			if(j===0){
+				cadenaInversa+=palabra[j-1];
+			}
+			else{
+				cadenaInversa+=palabra[j-1];
+			}
+		}
+		cadenaInversa+=" ";
+	}
+	return cadenaInversa;
 }
 
 function backwardsWords(cadena) {
-
+	return cadena.split(" ").reverse().join(" ");
 }
 
 function palindrome(cadena) {
+	let arrayPalindromo = cadena.split(" ").join("").split("");
+	let esPalindromo = true;
+
+	for(let i=0;i<arrayPalindromo.length;i++){
+		if((arrayPalindromo[i].toLowerCase())!==(arrayPalindromo[(arrayPalindromo.length-1-i)].toLowerCase()) && i!==(arrayPalindromo.length-1-i)){
+			esPalindromo=false;
+		}
+	}
+	if(esPalindromo){
+		return "Es palindromo";
+	}else{
+		return "No es palindromo";
+	}
 
 }
 
--- Cliente/DWC-Practica-JavaScript-Frase/scripts/index.js
+++ Cliente/DWC-Practica-JavaScript-Frase/scripts/index.js
@@ -1,3 +1,12 @@
 'use strict'
 
+let frase = prompt("Introduce una frase:");
 
+console.log(frase);
+console.log(`Tu frase tiene ${letters(frase)} caracteres`);
+console.log(`Tu frase tiene ${words(frase)} palabras`);
+console.log(upperString(frase));
+console.log(titleString(frase));
+console.log(backwardsLetters(frase));
+console.log(backwardsWords(frase));
+console.log(palindrome(frase));
