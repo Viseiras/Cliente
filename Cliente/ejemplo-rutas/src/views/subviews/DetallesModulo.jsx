084456779c6e8a8384e7c11452f1bc3904d642d9
Javier Vicedo <101125117+Viseiras@users.noreply.github.com>
2025-10-31 18:12:04 +0100

    Add files via upload

--- Cliente/ejemplo-rutas/src/views/subviews/DetallesModulo.jsx
+++ Cliente/ejemplo-rutas/src/views/subviews/DetallesModulo.jsx
@@ -1,5 +1,22 @@
 import { useParams } from "react-router-dom";
 
-function DetallesModulo(){
+function DetallesModulo(props){
     
+    const {moduleId} = useParams();
+    const moduloSeleccionado = props.modulos.find(m => m.id === moduleId)
+
+    if(!moduloSeleccionado){
+        return <p>Selecciona un modulo de la lista para ver sus detalles</p>
+    }
+    
+    return(
+        <div className="detalle-card">
+            <h3>Detalles: {moduloSeleccionado.nombre}</h3>
+            <p><strong>ID:</strong> {moduloSeleccionado.id.toUpperCase()}</p>
+            <p><strong>Carga Horaria: </strong> {moduloSeleccionado.horas} horas</p>
+            <p><strong>Descipción:</strong> {moduloSeleccionado.descripcion}</p>
+        </div>
+    )
 }
+
+export default DetallesModulo;
