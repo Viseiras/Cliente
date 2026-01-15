0c51083f08a74ab89cde5d1a97b92486b3241af8
Javier Vicedo <101125117+Viseiras@users.noreply.github.com>
2026-01-15 18:39:49 +0100

    Add files via upload

---
+++ Cliente/ejemplo-firebase-funcional/js/main.js
@@ -0,0 +1,46 @@
+"use strict";
+
+import { addIdea, listenIdeas } from "./ideasRepo.js";
+import { renderIdeas, flashMessage } from "./ui.js";
+
+window.onload = () => {
+  const form = document.getElementById("ideaForm");
+  const input = document.getElementById("ideaInput");
+  const list = document.getElementById("list");
+  const msg = document.getElementById("msg");
+
+  // 1) Activar listener en tiempo real
+  // Cada vez que haya cambios en Firestore se repinta la lista
+  const unsubscribe = listenIdeas((ideas)=>{
+    renderIdeas(list,ideas);
+  });
+
+  // 2) Evento submit del formulario: añadir idea
+  form.addEventListener(
+    "submit",
+    async (e) => {
+      e.preventDefault();
+
+      const text = input.value.trim();
+      if (!text) {
+        flashMessage(msg, "Escribe algo primero.");
+        return;
+      }
+
+      // Limpieza rápida de UI
+      input.value = "";
+      input.focus();
+      flashMessage(msg, "Enviando...");
+
+      //Añadir idea
+      try{
+        await addIdea(text);
+        flashMessage(msg,"Idea añadida");
+      }catch(error){
+        console.log(error);
+        flashMessage(msg,`${error.message}`);
+      }
+    },
+    false
+  );
+};
